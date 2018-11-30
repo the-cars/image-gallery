cac8452a73152cc6490bb4e4a5f904748a6fc1a2
Carmen V Ramos <carmenvramos@yahoo.com>
2018-11-29 16:21:47 -0800

    Modified AlbumDetail, albumsApi

--- src/components/albums/AlbumDetail.vue
+++ src/components/albums/AlbumDetail.vue
@@ -1,11 +1,22 @@
 <template>
-  <section> I am the Album Detail </section>
+  <section> I am the Album Detail 
+    <h2>{{album.make}}</h2>
+    <h3>Images</h3>
+    <p>
+      <button>To do later</button>
+    </p>  
+  </section>
+
 
 </template>
 
 <script>
-export default {
+import albumsApi from '../../services/albumsApi';
 
+export default {
+    created() {
+        this.album = albumsApi.getAlbum(this.$route.params.id);
+    }
 };
 </script>
 
--- src/services/albumsApi.js
+++ src/services/albumsApi.js
@@ -3,5 +3,8 @@ import albums from '../assets/data.js';
 export default {
     getAlbums() {
         return albums;
+    },
+    getAlbum() {
+        return albums;
     }
 };
