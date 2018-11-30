6d4eb79804aa07dfa2c8cc998d843a67af6dd34d
Carmen V Ramos <carmenvramos@yahoo.com>
2018-11-30 05:27:57 -0800

    Added styling on Thumbnails

--- src/components/albums/Thumbnails.vue
+++ src/components/albums/Thumbnails.vue
@@ -18,4 +18,42 @@ export default {
 
 <style>
 
+ul {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  grid-column-gap: 2px;
+  grid-row-gap: 2px; 
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
+li {
+  position: relative;
+  height: 300px;
+  width: 275px;
+  text-align: center;
+  border: 2px solid steelblue;
+}
+ul img {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 1;
+  object-fit: cover;
+}
+h4 {
+  position: absolute;
+  top: 0;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 40px;
+  font-size: .85em;
+  z-index: 10;
+  color:red;
+  font-weight: bold
+}
+
 </style>
