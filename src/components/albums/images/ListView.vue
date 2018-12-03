a77c822b2679e5ec98ad7c7da182a1ea14cdb819
Carmen V Ramos <carmenvramos@yahoo.com>
2018-12-02 18:46:35 -0800

    Added image title to ListViewer

--- src/components/albums/images/ListView.vue
+++ src/components/albums/images/ListView.vue
@@ -2,13 +2,12 @@
     <section>
     <h3>List View</h3>
         <p>There are {{images.length}} image(s)</p>
-    <!-- <ul>
+    <ul>
         <li v-for="(image, index) in images"
             :key="index">
         <h4>{{image.title}}</h4>
-        <img :src="image.urlImage">
         </li>
-    </ul> -->
+    </ul>
 </section>
 </template>
 
@@ -20,8 +19,29 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 
+ul  {
+  display: block;  
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+li {
+  height: 50px;
+  text-align: center;
+  border: 2px solid steelblue;
+  padding-top: 10px;
+}
 
+h4 {
+  top: 0;
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 20px;
+  font-size: .95em;
+  font-weight: bold;
+}
 
 </style>
--- src/components/albums/images/ThumbnailView.vue
+++ src/components/albums/images/ThumbnailView.vue
@@ -19,7 +19,7 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
 ul {
   display: grid;
   grid-template-columns: repeat(5, 1fr);
